aeef65f11670e22a5d1b1e4bd1d27c93daad594a
Arjun K <84978999+xorjun@users.noreply.github.com>
2023-01-10 14:10:59 +0530

    Add files via upload

---
+++ pages/footer.php
@@ -0,0 +1,12 @@
+    <br /></div>
+      <footer>
+        <div class="mt-auto d-flex justify-content-center">
+          <div class="col-lg-8">
+            <br />
+            <p>Protected by <strong><a href="https://zenter.in/s" target="_blank">RAKSHA</a></strong></p>
+          </div>
+        </div>
+      </footer>
+
+  </body>
+</html>
